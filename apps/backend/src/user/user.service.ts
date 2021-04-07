6530c12ab99c7c889226a04b05c52fa41587f5a9
EmilBuszylo <emil.buszylo@kiwijobs.pl>
2021-04-07 13:19:46 +0200

    change again

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -35,7 +35,7 @@ export class UserService {
 
   get(id: number): UserOutputDto {
     return {
-      name: 'test test',
+      name: 'test2',
       age: id,
       env: process.env.ENV_TEXT_GET,
     };
